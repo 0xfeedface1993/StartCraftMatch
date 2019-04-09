febb7612f550f6138c448f69282105e1c661d074
许庭淞 <virus1993@icloud.com>
2019-04-09 09:06:30 +0800

    初始化项目

--- Package.swift
+++ Package.swift
@@ -1,16 +1,20 @@
-// swift-tools-version:4.0
-
+// swift-tools-version:4.2
+// Generated automatically by Perfect Assistant
+// Date: 2019-04-09 00:55:30 +0000
 import PackageDescription
 
 let package = Package(
-	name: "PerfectTemplate",
+	name: "StartCraftMatch",
 	products: [
 		.executable(name: "PerfectTemplate", targets: ["PerfectTemplate"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", from: "3.0.0"),
+		.package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", "3.0.0"..<"4.0.0"),
+		.package(url: "https://github.com/SwiftORM/SQLite-StORM.git", "3.0.0"..<"4.0.0"),
+		.package(url: "https://github.com/PerfectlySoft/Perfect-Notifications.git", "4.0.0"..<"5.0.0"),
+		.package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", "3.0.0"..<"4.0.0")
 	],
 	targets: [
-		.target(name: "PerfectTemplate", dependencies: ["PerfectHTTPServer"])
+		.target(name: "PerfectTemplate", dependencies: ["PerfectHTTPServer", "PerfectNotifications", "SQLiteStORM", "PerfectLogger"])
 	]
 )
