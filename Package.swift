cda890d1f9599545973db20295c8371dc1a8e9e9
许庭淞 <virus1993@icloud.com>
2019-04-11 11:41:11 +0800

    Using library for dependencies

--- Package.swift
+++ Package.swift
@@ -1,6 +1,6 @@
 // swift-tools-version:4.2
 // Generated automatically by Perfect Assistant
-// Date: 2019-04-09 00:55:30 +0000
+// Date: 2019-04-09 08:17:42 +0000
 import PackageDescription
 
 let package = Package(
@@ -9,12 +9,9 @@ let package = Package(
 		.executable(name: "PerfectTemplate", targets: ["PerfectTemplate"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", "3.0.0"..<"4.0.0"),
-		.package(url: "https://github.com/SwiftORM/SQLite-StORM.git", "3.0.0"..<"4.0.0"),
-		.package(url: "https://github.com/PerfectlySoft/Perfect-Notifications.git", "4.0.0"..<"5.0.0"),
-		.package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", "3.0.0"..<"4.0.0")
+		.package(url: "https://github.com/0xfeedface1993/StarCraftMatchCore.git", from: "0.1.1")
 	],
 	targets: [
-		.target(name: "PerfectTemplate", dependencies: ["PerfectHTTPServer", "PerfectNotifications", "SQLiteStORM", "PerfectLogger"])
+		.target(name: "PerfectTemplate", dependencies: ["StarCraftMatchCore"])
 	]
 )
--- Sources/PerfectTemplate/main.swift
+++ Sources/PerfectTemplate/main.swift
@@ -19,6 +19,7 @@
 
 import PerfectHTTP
 import PerfectHTTPServer
+import StarCraftMatchCore
 
 // An example request handler.
 // This 'handler' function can be referenced directly in the configuration below.
@@ -27,21 +28,45 @@ func handler(request: HTTPRequest, response: HTTPResponse) {
 	response.setHeader(.contentType, value: "text/html")
 	response.appendBody(string: "<html><title>Hello, world!</title><body>Hello, world!</body></html>")
 	// Ensure that response.completed() is called when your processing is done.
+    save(zone: "美洲") { (isSuccess) in
+        if !isSuccess {
+            log(error: "*** 插入或更新赛区数据失败")
+        }   else {
+            log(message: "插入成功！")
+        }
+    }
 	response.completed()
 }
 
+func testTeamDB() {
+    deleteAllTeamRecords()
+    deleteAllZoneRecords()
+    deleteAllTeamZoneRecords()
+    
+    save(zone: "美洲") { (isSuccess) in
+        if !isSuccess {
+            log(error: "*** 插入或更新赛区数据失败")
+        }   else {
+            log(message: "插入成功！")
+        }
+    }
+}
+
 // Configure one server which:
-//	* Serves the hello world message at <host>:<port>/
-//	* Serves static files out of the "./webroot"
-//		directory (which must be located in the current working directory).
-//	* Performs content compression on outgoing data when appropriate.
+//    * Serves the hello world message at <host>:<port>/
+//    * Serves static files out of the "./webroot"
+//        directory (which must be located in the current working directory).
+//    * Performs content compression on outgoing data when appropriate.
 var routes = Routes()
 routes.add(method: .get, uri: "/", handler: handler)
 routes.add(method: .get, uri: "/**",
-		   handler: StaticFileHandler(documentRoot: "./webroot", allowResponseFilters: true).handleRequest)
+           handler: StaticFileHandler(documentRoot: "./webroot", allowResponseFilters: true).handleRequest)
+configDatabase()
+
+
 try HTTPServer.launch(name: "localhost",
-					  port: 8181,
-					  routes: routes,
-					  responseFilters: [
-						(PerfectHTTPServer.HTTPFilter.contentCompression(data: [:]), HTTPFilterPriority.high)])
+                      port: 8181,
+                      routes: routes,
+                      responseFilters: [
+                        (PerfectHTTPServer.HTTPFilter.contentCompression(data: [:]), HTTPFilterPriority.high)])
 
